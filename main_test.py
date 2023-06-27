eb55a2d8b9fada3666624494bc080343b58c32da
Kyuwoong Lee <kwooler@gmail.com>
2023-06-27 16:03:56 -0700

    Update main_test.py

--- main_test.py
+++ main_test.py
@@ -7,38 +7,35 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '5\n4\n3\n2\n1\n5\n'
+    datastr = '10\n20'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
+    assert ret == [11, 13, 17, 19]
+
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*5[\w,\W]*4[\w,\W]*3[\w,\W]*2[\w,\W]*1[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    # regex_string = r'[\w,\W]*5[\w,\W]*4[\w,\W]*3[\w,\W]*2[\w,\W]*1[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    # assert res != None
+    # print(res.group())
 
 
 def test_main_2():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '100\n90\n110'
+    datastr = '20\n50'
     sys.stdin = io.StringIO(datastr)
 
-    main.main()
+    ret = main.main()
     sys.stdout = sys.__stdout__
     print('Captured ', captureOut.getvalue())
     lines = captureOut.getvalue().split('\n')
     print(lines)
 
-    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
-    # res = re.search(regex_string, lines[0])
-    regex_string = r'[\w,\W]*100[\w,\W]*90[\w,\W]*'
-    res = re.search(regex_string, lines[0])
-    assert res != None
-    print(res.group())
+    assert ret == [23, 29, 31, 37, 41, 43, 47]
