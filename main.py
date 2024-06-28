2781be0fa4c19132ef85069064634467d173ff0f
Sophie Hanchey <sophiehanchey@gmail.com>
2024-06-28 15:12:32 -0700

    testing user input code

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 def main():
     plist = []
-    ##################################################
-    # Comlete your code here
-    ##################################################
+    
+    firstnum = int(input('Please input a number: '))
+    
+    # avoid error if first num is smaller than second
+    while True:
+        secondnum = int(input('Please enter a number smaller than the first: '))
+        
+        if(secondnum>=firstnum):
+            print('Error, the second number must be smaller than the first')
+            continue
+        else:
+            break
 
     return plist
 
